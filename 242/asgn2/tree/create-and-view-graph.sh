3348c52f8e881b8182311ab1b3c4d35dca88785a
les <leslie@l3slie.lan>
2009-10-06 16:21:28 +1300

    only want 212 assignment in here for now

--- 242/asgn2/tree/create-and-view-graph.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-FILENAME=${1:-dot-tree}
-dot -Tfig < ${FILENAME} >| ${FILENAME}.fig && \
-   fig2dev -L pdf ${FILENAME}.fig ${FILENAME}.pdf && \
-   open ${FILENAME}.pdf
--- 242/lab4/test.sh
+++
@@ -1,3 +0,0 @@
-for((i = 0; i < 10000; i++)); do
-	echo $RANDOM
-done
